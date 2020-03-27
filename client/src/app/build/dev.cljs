e9b1f28a6d3baed1705b7d66255aa50dd583f7e1
panthevm <panthevm@yandex.ru>
2020-03-27 23:14:00 +0300

    Add chartjs

--- client/src/app/build/dev.cljs
+++ client/src/app/build/dev.cljs
@@ -2,8 +2,7 @@
   (:require [app.core      :as core]
             [re-frisk.core :as frisk]))
 
-(defn ^:before-load devtools []
-  (frisk/enable-re-frisk! {:width "400px" :height "500px"}))
+(frisk/enable-re-frisk! {:width "400px" :height "500px"})
 
 (defn ^:after-load render [] (core/mount))
 
