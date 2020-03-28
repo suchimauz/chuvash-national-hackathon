6c38ce92b81223daa699d9aef0c1136f27cc40f1
rkz1337 <booster2228@yandex.ru>
2020-03-28 11:09:41 +0300

    Add GothamPro font and favicon

--- client/src/app/styles.cljs
+++ client/src/app/styles.cljs
@@ -6,8 +6,12 @@
 
 (def style
   (styles
-   [:.app
-    [:.pointer {:cursor :pointer}]
-    [:span
-     [:btn {:cursor :pointer}]]
-    [:.separator {:height "100px"}]]))
+   [:body {:color "#333" :font-size "15px" :font-family "GothamPro" :height "100%"}
+    [:.app
+     [:h1 {:font-size "32px" :font-family "GothamPro-bold" :font-weight "900"}]
+     [:h2 {:font-size "24px" :font-family "GothamPro-medium" :font-weight "900"}]
+     [:b {:font-size "16px" :font-family "GothamPro"}]
+     [:.pointer {:cursor :pointer}]
+     [:span
+      [:btn {:cursor :pointer}]]
+     [:.separator {:height "100px"}]]]))
