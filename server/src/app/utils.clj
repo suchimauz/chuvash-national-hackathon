491f660315bfbd221137de2e2544912d9e32ab5e
suchimauz <alexey.fedulov98@gmail.com>
2020-03-29 17:10:54 +0300

    Fix event

--- server/src/app/utils.clj
+++ server/src/app/utils.clj
@@ -94,7 +94,7 @@
       __project_district {:join _p_d
                           :group-by :project.id}
       __event_district   {:join _e_d
-                          :group-by :project.id})))
+                          :group-by :event.id})))
 
 (defn where-params [{:keys [table]} {:keys [id ilike] :as params}]
   (let [conds (cond-> []
